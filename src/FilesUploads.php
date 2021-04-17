e7528e9f4c3fdeaab596b9592f6dbea44820b7dd
Pawan <pawan.garg@sufalamtech.com>
2021-04-17 14:39:14 +0530

    all

--- src/FilesUploads.php
+++ src/FilesUploads.php
@@ -2,32 +2,20 @@
 
 namespace hum\mediafile;
 use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Validator;
 class FilesUploads
 {
     public function postImages($images,$storeurl)
-    {
-        $validator = Validator::make($images, [
-            'images' => 'required|mimes:jpg,jpeg,png,bmp|max:2048'
-        ]);
-        $responsearr = $imgarray = array();
-        if ($validator->fails()) {
-            //return error if validation fails
-            $errors = $validator->errors();
-            $responsearr['output']['status'] = 'failure';
-            $responsearr['output']['message'] = $errors;
-        } else{
-            //for multiple file upload
-            foreach($images as $key=>$file){
-                $profileImage = time().$file->getClientOriginalName();
-                $file->move($storeurl, $profileImage);
-                $imgarray[] = $profileImage;
-            }
-            // Upload Orginal Image  
-            $responsearr['output']['images'] =  $imgarray;        
-            $responsearr['output']['status'] = 'Success';
-            $responsearr['output']['message'] = 'Images Uploded Successfully.';
-        }                    
+    {                    
+        //for multiple file upload
+        foreach($images as $key=>$file){
+            $profileImage = time().$file->getClientOriginalName();
+            $file->move($storeurl, $profileImage);
+            $imgarray[] = $profileImage;
+        }
+        // Upload Orginal Image  
+        $responsearr['output']['images'] =  $imgarray;        
+        $responsearr['output']['status'] = 'Success';
+        $responsearr['output']['message'] = 'Uploded Successfully.';
         return response()->json($responsearr);
     }
 }
